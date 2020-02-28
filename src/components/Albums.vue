c1727f24991e8a1aaff25da071c7ef1de9f3c620
ctrlouis <louis.bty57@gmail.com>
2020-02-28 17:47:23 +0100

    create albums list components

---
+++ src/components/Albums.vue
@@ -0,0 +1,22 @@
+<!-- ALBUMS LIST DISPLAYER COMPONENTS -->
+
+<template>
+    <div>
+        <album v-for="album in albums['release-groups']" :data="album"></album>
+    </div>
+</template>
+
+<script>
+import Album from './Album.vue';
+
+
+export default {
+    props: ['albums'],
+
+    components: { Album }
+}
+</script>
+
+<style lang="scss">
+
+</style>
