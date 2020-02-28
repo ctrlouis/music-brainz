1baed7d56cf45eafdad179f5bdfb475c0be32e1a
ctrlouis <louis.bty57@gmail.com>
2020-02-28 17:47:51 +0100

    create album component

---
+++ src/components/Album.vue
@@ -0,0 +1,43 @@
+<!-- ALBUM DISPLAYER COMPONENTS -->
+
+<template>
+    <div>
+        <div>
+            <img :src="src" alt="Front album cover">
+        </div>
+        <div>
+            <h2>{{data.title}}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['data'],
+
+    data() {
+        return {
+            api: {
+                cover: {
+                    url: "http://coverartarchive.org/release/"
+                }
+            }
+        }
+    },
+
+    computed: {
+        src() {
+            return this.api.cover.url + this.data.id + "/front";
+        }
+    },
+
+    created() {
+        console.log(this.data);
+        
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
