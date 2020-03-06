23e40055cc1437c8386451d7f634fe29c568d527
ctrlouis <louis.bty57@gmail.com>
2020-03-06 12:56:36 +0100

    Create search form component

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <form @submit="exec">
+            <input type="text" placeholder="Search..." v-model="research">
+            <select v-model="categorie">
+                <option disabled value="">Toutes catégories</option>
+                <option value="album">Albums</option>
+                <option value="artist">Artists</option>
+                <option value="track">Tracks</option>
+            </select>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+
+    data() {
+        return {
+            research: "",
+            categorie: ""
+        }
+    },
+
+    methods: {
+        exec(event) {
+            event.preventDefault();
+            if (this.research != "") {
+                this.$emit('newSearch', this.research);
+                this.clearForm();
+            }
+        },
+
+        clearForm() {
+            this.research = "";
+            this.categorie = "";
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+</style>
