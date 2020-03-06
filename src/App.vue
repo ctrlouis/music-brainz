f663995d76f228776616e370ee782e272c1fc675
ctrlouis <louis.bty57@gmail.com>
2020-03-06 12:53:57 +0100

    use vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,143 +1,23 @@
 <!-- MAIN COMPONENT -->
 
-
 <template>
   <div id="app">
-    <form @submit="exec">
-      <input type="text" placeholder="Search..." v-model="searched">
-      <br>
-      <input type="radio" id="album" value="album" v-model="searchType">
-      <label for="two">Album</label>
-      <br>
-      <input type="radio" id="artist" value="artist" v-model="searchType">
-      <label for="one">Artist</label>
-      <br>
-      <input type="radio" id="track" value="track" v-model="searchType">
-      <label for="two">Track</label>
-      <br>
-    </form>
-
-    <liste v-if="fetchData.type == 'album'" :albums="fetchData.data"></liste>
-    <!-- <liste v-else-if="fetchData.type == 'artist'" :artists="fetchData.data"></liste> -->
-    <!-- <liste v-else-if="fetchData.type == 'track'" :tracks="fetchData.data"></liste> -->
+    <h1>My Music Manager</h1>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-import Liste from './components/Liste.vue';
 
 
 export default {
   name: 'app',
 
-  components: { Liste },
-
   data() {
     return {
-      api: {
-        music: {
-          url: "http://musicbrainz.org/ws/2"
-        }
-      },
-      searched: "",
-      searchType: "",
-      fetchData: {
-        type: '',
-        data: []
-      }
     }
-  },
-
-  methods: {
-    exec(event) {
-      event.preventDefault();
-      switch (this.searchType) {
-        case "album":
-          this.fetchAlbums(this.searched);
-          break;
-        case "artist":
-          this.fetchArtists(this.searched);
-          break;
-        case "track":
-          this.fetchTracks(this.searched);
-          break;
-      
-        default:
-          break;
-      }
-    },
-
-    /*
-    * Retrieve albums from music-brainz api
-    */
-    fetchAlbums(page=0) {
-      const url = this.api.music.url + "/release-group?query=" + this.searched + "&" + this.calculPage(page);
-      axios.get(url)
-        .then((res) => {
-          const albumsData = res.data;
-          this.updateData(albumsData);
-          this.fetchData.type = 'album';
-          console.log(albumsData);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    },
-
-    /*
-    * Retrieve artists from music-brainz api
-    */
-    fetchArtists(page=0) {
-      const url = this.api.music.url + "/artist?query=" + this.searched  + "&" + this.calculPage(page);;
-      axios.get(url)
-        .then((res) => {
-          const artistsData = res.data;
-          this.fetchData.type = 'artist';
-          console.log(artistsData);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    },
-
-    /*
-    * Retrieve tracks from music-brainz api
-    */
-    fetchTracks(page=0) {
-      const url = this.api.music.url + "/release?query=" + this.searched + "&" + this.calculPage(page);
-      axios.get(url)
-        .then((res) => {
-          const tracksData = res.data;
-          this.fetchData.type = 'track';
-          console.log(tracksData);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    },
-
-    /*
-    * Calcule page and offest query options
-    */
-    calculPage(pageNbr=0, pageSize=100) {
-      const offset = pageNbr * pageSize;
-      return "limite=" + pageSize + "&offset=" + offset;
-    },
-
-    /*
-    * Retrieve tracks from music-brainz api
-    */
-    updateData(datas) {
-      // console.log(datas);
-      this.fetchData.data = datas;
-      console.log(this.fetchData.data);
-      // while (this.fetchData.data.length > 0) this.fetchData.data.unshift();
-      // datas.forEach(data => this.fetchData.data.push(data));
-    }
-  },
-
-  created() {}
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,35 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+// import routing from './confs/routing.js';
+import App from './App.vue';
+import HomePage from './components/HomePage.vue';
+import Search from './components/Search.vue';
+import NotFound from './components/NotFound.vue';
+
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+
+  routes: [
+    {
+      path: "/",
+      component: HomePage
+    },
+    {
+      path: "/search/:research",
+      component: Search
+    },
+    {
+      path: "*",
+      component: NotFound
+    }
+  ]
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
