7c5a83d815da39cf9fd7b30bacffde4dbc7cf64e
ctrlouis <louis.bty57@gmail.com>
2020-03-13 14:44:36 +0100

    artist data fetching in artist details

--- src/components/ArtistDetails.vue
+++ src/components/ArtistDetails.vue
@@ -5,6 +5,9 @@
 </template>
 
 <script>
+import axios from 'axios';
+
+
 export default {
     data() {
         return {
@@ -13,13 +16,20 @@ export default {
                     url: "http://musicbrainz.org/ws/2"
                 }
             },
+            artist: null
         }
     },
 
     methods: {
         fetchArtistData() {
-            const url = `${this.api.music.url}/artists/${this.$route.params.id}`;
-
+            const url = `${this.api.music.url}/artist/${this.$route.params.id}`;
+            console.log(url);
+            
+            axios.get(url)
+            .then((res) => {
+                this.artist = res.data;
+                console.log(this.artist);
+            }).catch(err => console.error(err));
         }
     },
 
