2a32b2f4b2840ef53dc204b59f087b51536e523a
ctrlouis <louis.bty57@gmail.com>
2020-03-13 15:15:27 +0100

    add albums data fetching

--- src/components/ArtistDetails.vue
+++ src/components/ArtistDetails.vue
@@ -1,40 +1,62 @@
 <template>
     <div>
-        <p>Artist details</p>
+        <div v-if="artist">
+            <h1>{{artist.name}}</h1>
+        </div>
+        <p v-else>Loading</p>
+
+        <!-- <Album v-if="albums" v-for="album in albums" :data="album"></Album> -->
+        <!-- <p v-else>Loading</p> -->
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Album from './Album.vue';
+import Track from './Track.vue';
 
 
 export default {
+    components: { Album, Track },
+
     data() {
         return {
+            id: null,
+            artist: null,
+            albums: null,
             api: {
                 music: {
-                    url: "http://musicbrainz.org/ws/2"
+                    url: "https://musicbrainz.org/ws/2"
                 }
-            },
-            artist: null
+            }
         }
     },
 
     methods: {
         fetchArtistData() {
-            const url = `${this.api.music.url}/artist/${this.$route.params.id}`;
-            console.log(url);
+            const url = `${this.api.music.url}/artist/${this.$route.params.id}?fmt=json`;
             
             axios.get(url)
             .then((res) => {
                 this.artist = res.data;
                 console.log(this.artist);
             }).catch(err => console.error(err));
+        },
+        fetchArtistAlbums() {
+            const url = `${this.api.music.url}/release?artist=${this.$route.params.id}&fmt=json&limit=10`;
+            console.log(url);
+            axios.get(url)
+            .then((res) => {
+                this.albums = res.data;
+                console.log(this.albums);                
+            }).catch(err => console.error(err));
         }
     },
 
     created() {
-        this.fetchArtistData();        
+        this.id = this.$route.params.id;
+        this.fetchArtistData();
+        this.fetchArtistAlbums();
     }
 }
 </script>
