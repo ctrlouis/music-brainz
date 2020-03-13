f4442e9dccb51fa6b5789d253771c5e4cdcc813a
ctrlouis <louis.bty57@gmail.com>
2020-03-13 14:34:44 +0100

    init artist details component

---
+++ src/components/ArtistDetails.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p>Artist details</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['data'],
+
+    data() {
+        return {
+            api: {
+                music: {
+                    url: "http://musicbrainz.org/ws/2"
+                }
+            },
+        }
+    },
+
+    methods: {
+        fetchArtistData() {
+            const url = `${this.api.music.url}/artists/${this.$route.params.id}`;
+        }
+    },
+
+    created() {
+        this.fetchArtistData();        
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
