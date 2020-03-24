fe9e4c3d3c3e9a4f718556fb8f5c8f54ed303f20
ctrlouis <louis.bty57@gmail.com>
2020-03-24 15:14:50 +0100

    add data displayed

--- src/components/Artist.vue
+++ src/components/Artist.vue
@@ -3,6 +3,18 @@
 <template>
     <div class="col-6 col-s-4 col-l-3 col-xl-2">
         <h3>{{data.name}}</h3>
+        <div v-if="data.type" class="info">
+            <div>Type:</div>
+            <div>{{data.type}}</div>
+        </div>
+        <div v-if="data.gender" class="info">
+            <div>Gender:</div>
+            <div>{{data.gender}}</div>
+        </div>
+        <div v-if="data.country" class="info">
+            <div>Country:</div>
+            <div>{{data.country}}</div>
+        </div>
         <router-link :to="detailUrl">Go to details</router-link>
     </div>
 </template>
