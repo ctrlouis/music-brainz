47961ebdadf30956c5b98888bf3eef3af0f15397
ctrlouis <louis.bty57@gmail.com>
2020-03-24 02:32:39 +0100

    add track in album details

--- src/components/AlbumDetails.vue
+++ src/components/AlbumDetails.vue
@@ -1,14 +1,26 @@
 <template>
     <div>
-        <div v-if="album">
-            <h1>{{album.title}}</h1>
+        <div v-if="loaded">
+            <h1>{{fetchData.album.data.title}}</h1>
+            <p>By <span v-for="artist in artists"><router-link :to="`/artists/${artist.id}`">{{ artist.name }}</router-link></span></p>
             <h2>Tracks</h2>
-            <Track v-for="track in album.releases" :data="track"></Track>
+            <div v-if="fetchData.tracks">
+                <table>
+                    <thead>
+                        <tr>
+                            <th><h4>Title</h4></th>
+                            <th><h4>Album</h4></th>
+                            <th><h4>Artist</h4></th>
+                            <th><h4>Time</h4></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <Track v-for="track in fetchData.tracks.data.recordings" :data="track"></Track>
+                    </tbody>
+                </table>
+            </div>
         </div>
         <div v-else><Loader></Loader></div>
-
-        <!-- <Album v-if="albums" v-for="album in albums" :data="album"></Album>
-        <p v-else>Loading</p> -->
     </div>
 </template>
 
@@ -18,14 +30,16 @@ import Album from './Album.vue';
 import Track from './Track.vue';
 import Loader from './Loader.vue';
 
-
 export default {
     components: { Album, Track, Loader },
 
     data() {
         return {
             id: null,
-            album: null,
+            fetchData: {
+                album: null,
+                tracks: null
+            },
             api: {
                 music: {
                     url: "https://musicbrainz.org/ws/2"
@@ -35,19 +49,44 @@ export default {
     },
 
     methods: {
-        fetchAlbumTracks() {
-            const url = `${this.api.music.url}/release-group/${this.id}?inc=releases`;
-            axios.get(url)
-            .then((res) => {
-                this.album = res.data;
-                console.log(this.album);
-            }).catch(err => console.error(err));
+        searchAll(research) {
+            const requests = [this.getAlbumRequest(), this.getTracksRequest()];
+            axios.all(requests)
+            .then(axios.spread((album, tracks) => {
+                this.fetchData.album = album;
+                console.log(this.fetchData.album);
+                this.fetchData.tracks = tracks;
+                console.log(this.fetchData.tracks);
+            })).catch(err => console.error(err));
+        },
+
+        getAlbumRequest() {
+            const url = this.api.music.url + "/release/" + this.id + "?fmt=json";
+            return axios.get(url);
+        },
+
+        getTracksRequest() {
+            const url = this.api.music.url + "/recording/?release=" + this.id + "&fmt=json";
+            return axios.get(url);
+        },
+    },
+
+    computed: {
+        artists() {
+            let artists = [];
+            if (this.fetchData.album.data['artist-credit']) {
+                this.fetchData.album.data['artist-credit'].forEach(artist => artists.push(artist.artist));
+            }
+            return artists;
+        },
+        loaded() {
+            return this.fetchData.album || this.fetchData.tracks;
         }
     },
 
     created() {
         this.id = this.$route.params.id;
-        this.fetchAlbumTracks();
+        this.searchAll();
     }
 }
 </script>
