376ad5d0bbc34509ac859a9aeb380c4c6b5b1d7c
ctrlouis <louis.bty57@gmail.com>
2020-03-24 22:23:54 +0100

    add artist tracks

--- src/components/ArtistDetails.vue
+++ src/components/ArtistDetails.vue
@@ -18,10 +18,29 @@
                 <div class="cards">
                     <Album v-for="album in fetchData.albums.data.releases" :data="album"></Album>
                 </div>
-                <Loader v-if="loading && !error.set"></Loader>
-                <Error v-if="error.set">{{error.message}}</Error>
-                <button v-if="dataLeft" class="button" @click="loadMore">Load more</button>
+                <Loader v-if="lastSearch == 'albums' && loading && !error.set"></Loader>
+                <Error v-if="lastSearch == 'albums' && error.set">{{error.message}}</Error>
+                <button v-if="albumLeft" class="button" @click="loadMoreAlbums">Load more</button>
             </div>
+            <div v-if="fetchData.tracks">
+                <h2><i class="fas fa-music"></i> Tracks :</h2>
+                
+                <table>
+                    <thead>
+                        <tr>
+                            <th><h4>Title</h4></th>
+                            <th><h4>Time</h4></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <Track v-for="track in fetchData.tracks.data.recordings" :data="track" :info="{title: true, time: true}"></Track>
+                    </tbody>
+                </table>
+                <Loader v-if="lastSearch == 'tracks' && loading && !error.set"></Loader>
+                <Error v-if="lastSearch == 'tracks' && error.set">{{error.message}}</Error>
+                <button v-if="trackLeft" class="button" @click="loadMoreTracks">Load more</button>
+            </div>
+            
         </div>
         <div v-else-if="!error.set"><Loader></Loader></div>
         <Error v-if="error.set && !mainDataLoad">{{error.message}}</Error>
@@ -46,8 +65,14 @@ export default {
             fetchData: {
                 artist: null,
                 albums: null,
-                count: 0
+                tracks: null,
+                albumCount: 0
             },
+            pageNbr: {
+                albums: 0,
+                tracks: 0
+            },
+            lastSearch: "",
             api: {
                 music: {
                     url: "https://musicbrainz.org/ws/2"
@@ -63,12 +88,14 @@ export default {
 
     methods: {
         searchAll(research) {
-            const requests = [this.getArtistsRequest(), this.getAlbumsRequest()];
+            const requests = [this.getArtistsRequest(), this.getAlbumsRequest(), this.getTracksRequest()];
             axios.all(requests)
-            .then(axios.spread((artist, albums) => {
+            .then(axios.spread((artist, albums, tracks) => {
                 this.fetchData.artist = artist;
                 this.fetchData.albums = albums;
-                this.fetchData.count = albums.data['release-count'];
+                this.fetchData.albumCount = albums.data['release-count'];
+                this.fetchData.tracks = tracks;
+                this.fetchData.trackCount = tracks.data['recording-count'];
             })).catch(err => this.setError());
         },
 
@@ -78,15 +105,33 @@ export default {
         },
 
         getAlbumsRequest() {
-            const url = this.api.music.url + "/release?artist=" + this.id + "&fmt=json" + this.calculPage();
+            const url = this.api.music.url + "/release?artist=" + this.id + "&fmt=json" + this.calculPage(this.pageNbr.albums);
             return axios.get(url);
         },
 
-        loadMore() {
+        getTracksRequest() {
+            const url = this.api.music.url + "/recording?artist=" + this.id + "&fmt=json" + this.calculPage(this.pageNbr.tracks);
+            return axios.get(url);
+        },
+
+        loadMoreAlbums() {
             this.error.set = false;
             this.loading = true;
+            this.lastSearch = "albums";
+            this.pageNbr.albums++;
             this.getAlbumsRequest()
             .then(res => res.data.releases.forEach(album => this.fetchData.albums.data.releases.push(album)))
+            .catch(err => this.setError())
+            .finally(() => this.loading = false);
+        },
+
+        loadMoreTracks() {
+            this.error.set = false;
+            this.loading = true;
+            this.lastSearch = "tracks";
+            this.pageNbr.tracks++;
+            this.getTracksRequest()
+            .then(res => res.data.recordings.forEach(track => this.fetchData.tracks.data.recordings.push(track)))
             .catch(err => this.setError())
             .finally(() => this.loading = false);
         },
@@ -107,8 +152,12 @@ export default {
             return this.fetchData.artist && this.fetchData.albums;
         },
 
-        dataLeft() {
-            return this.fetchData.albums.data.releases.length < this.fetchData.count;
+        albumLeft() {
+            return this.fetchData.albums.data.releases.length < this.fetchData.albumCount;
+        },
+
+        trackLeft() {
+            return this.fetchData.tracks.data.recordings.length < this.fetchData.trackCount;
         }
     },
 
