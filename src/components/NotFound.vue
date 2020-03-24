73a89e363341b1aab19bf76f025514a10c8497a3
ctrlouis <louis.bty57@gmail.com>
2020-03-24 20:24:02 +0100

    Add link for return to main page when 404 page and redirect by timer

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,30 +1,41 @@
 <template>
-    <div>
-        <h1>404 Not Found : go somewhere else (lol)</h1>
-    </div>
+  <div>
+    <h1>404 Not Found</h1>
+    <router-link to="/"><h2>Return to the app</h2></router-link>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'app',
-    data () {
-        return {}
-    }
+  name: 'app',
+
+  data () {
+    return {}
+  },
+
+  created() {
+    setTimeout(() => this.$router.replace("/"), 5000);
+  }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 h1, h2 {
   font-weight: normal;
+  text-align: center;
+}
+
+h2 {
+  color: #42b983;
+  margin: 2em 0 4em 0;
 }
 
 ul {
