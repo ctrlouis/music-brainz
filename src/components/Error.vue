53add8e1bfc188572bc6c0bb5c26a275fa25cf4c
ctrlouis <louis.bty57@gmail.com>
2020-03-24 20:48:25 +0100

    create error vue

---
+++ src/components/Error.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="error">
+        <div>
+            <i class="fas fa-times"></i>
+        </div>
+        <div>
+            <p><slot></slot></p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#error {
+    margin: 4em 0;
+    display: flex;
+    flex-direction: column;
+
+    & > div {
+        display: flex;
+        justify-content: center;
+    }
+}
+
+i, p {
+    color: #b71c1c;
+    text-align: center;
+    font-size: 3em;
+    font-weight: bold;
+}
+
+i {
+    font-size: 5em;
+}
+</style>
